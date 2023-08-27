a2db9c8c025caf25058195e12b13c10bf38e70bc
hey-shashikant <shashikantsolanki041@gmail.com>
2023-08-27 14:21:33 +0530

    created basic sprint boot application

---
+++ src/test/java/ShashikantSpringBootApplication/ShashikantSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ShashikantSpringBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShashikantSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
